0798401a1ec7a1b8f91c98d6b7f3f472d1aca14d
Giulia <giuliacarolina912@gmail.com>
2025-04-23 00:31:46 -0300

    ,

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-document.getElementById("botao-acessibilidade").addEventListener("click", () => {
-    const opcoes = document.getElementById("opcoes-acessibilidade");
-    opcoes.classList.toggle("apresenta-lista");
+document.getElementById('aumentar-fonte').addEventListener('click', function() {
+    document.body.style.fontSize = '1.2em';
 });
 
-document.getElementById("aumentar-fonte").addEventListener("click", () => {
-    document.body.style.fontSize = "larger";
-});
-
-document.getElementById("diminuir-fonte").addEventListener("click", () => {
-    document.body.style.fontSize = "initial";
-});
-
-// Ativar/desativar modo contraste ao pressionar "C" no teclado
-document.addEventListener("keydown", (e) => {
-    if (e.key.toLowerCase() === "c") {
-        document.body.classList.toggle("contraste-alto");
-    }
+document.getElementById('diminuir-fonte').addEventListener('click', function() {
+    document.body.style.fontSize = '1em';
 });
